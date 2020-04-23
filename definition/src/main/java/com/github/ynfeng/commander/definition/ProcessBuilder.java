7dfedebcb453eba03cea7fd74ca32760ee13fa40
FengYannan <ynfeng666@gmail.com>
2020-04-23 20:57:40 +0800

    modify dependencies.

---
+++ definition/src/main/java/com/github/ynfeng/commander/definition/ProcessBuilder.java
@@ -0,0 +1,6 @@
+package com.github.ynfeng.commander.definition;
+
+public class ProcessBuilder {
+
+
+}
---
+++ definition/src/test/java/com/github/ynfeng/commander/definition/ProcessBuilderTest.java
@@ -0,0 +1,21 @@
+package com.github.ynfeng.commander.definition;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+
+public class ProcessBuilderTest {
+
+    @Test
+    public void should_build_one_node_process() {
+//        ProcessDefinition processDefinition = ProcessBuilder
+//            .name("testProcess")
+//            .version(1)
+//            .start().withParameters(1, 2, 3)
+//            .remoteNode("aService", "ref", 1)
+//            .withParameters(1, 2, 3)
+//            .end();
+    }
+}
