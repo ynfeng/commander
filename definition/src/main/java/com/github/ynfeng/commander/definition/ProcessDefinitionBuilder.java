4d834bbff3b96b82149ab98c7c4fb7fbe7e915c5
FengYannan <ynfeng666@gmail.com>
2020-04-25 13:06:44 +0800

    add checkstyle

--- definition/src/main/java/com/github/ynfeng/commander/definition/ProcessDefinitionBuilder.java
+++ definition/src/main/java/com/github/ynfeng/commander/definition/ProcessDefinitionBuilder.java
@@ -1,10 +1,10 @@
 package com.github.ynfeng.commander.definition;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ProcessDefinitionBuilder {
+    private ProcessDefinitionBuilder() {
+    }
+
     public static StartDefinitionBuilder create(String name, int version) {
         ProcessDefinition processDefinition = new ProcessDefinition(name, version);
         return new StartDefinitionBuilder(processDefinition);
