7de000428df24ef01fc144bcc21642524263634f
ynfeng <fynwin@gmail.com>
2020-08-05 10:41:19 +0800

    asjust code.

--- server/src/main/java/com/github/ynfeng/commander/server/ServerBuilder.java
+++ server/src/main/java/com/github/ynfeng/commander/server/ServerBuilder.java
@@ -3,12 +3,12 @@ package com.github.ynfeng.commander.server;
 public class ServerBuilder {
     private final ServerConfig config = new ServerConfig();
 
-    public Server build() {
-        return new Server(config);
-    }
-
     public ServerBuilder withName(String name) {
         config.setName(name);
         return this;
+    }
+
+    public Server build() {
+        return new Server(config);
     }
 }
