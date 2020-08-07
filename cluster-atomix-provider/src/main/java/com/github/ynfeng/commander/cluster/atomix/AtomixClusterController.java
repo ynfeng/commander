2c4c28e97ea9bcb2f2a753aa6b557dd58c699ed7
ynfeng <fynwin@gmail.com>
2020-08-07 22:18:11 +0800

    modify port.

--- cluster-atomix-provider/src/main/java/com/github/ynfeng/commander/cluster/atomix/AtomixClusterController.java
+++ cluster-atomix-provider/src/main/java/com/github/ynfeng/commander/cluster/atomix/AtomixClusterController.java
@@ -22,12 +22,12 @@ public class AtomixClusterController extends AbstractClusterController {
     @Override
     public void start() {
         Atomix atomix = Atomix.builder()
-            .withAddress(Address.from(nodeConfig.address(), nodeConfig.port()))
+            .withAddress(Address.from(nodeConfig.address(), 18089))
             .withMembershipProvider(BootstrapDiscoveryProvider.builder()
                 .withNodes(
                     Node.builder()
                         .withId(nodeConfig.nodeId())
-                        .withAddress(Address.from(nodeConfig.address(), nodeConfig.port()))
+                        .withAddress(Address.from(nodeConfig.address(), 18089))
                         .build())
                 .build())
             .build();
