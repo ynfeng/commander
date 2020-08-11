469197c80a85ed68d62e0b77f6c68de5640c0ef1
ynfeng <fynwin@gmail.com>
2020-08-11 13:09:36 +0800

    bootstrap unit test.

--- bootstrap/src/test/java/com/github/ynfeng/commander/bootstrap/BootrstapTest.java
+++ bootstrap/src/test/java/com/github/ynfeng/commander/bootstrap/BootrstapTest.java
@@ -1,33 +1,29 @@
 package com.github.ynfeng.commander.bootstrap;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
 
+import com.github.ynfeng.commander.cluster.Cluster;
+import com.github.ynfeng.commander.cluster.ClusterProvider;
 import com.github.ynfeng.commander.cluster.ClusterProviderLoader;
-import com.github.ynfeng.commander.cluster.SPIClusterProviderLoader;
 import com.github.ynfeng.commander.cluster.config.ClusterConfig;
 import com.github.ynfeng.commander.cluster.config.NodeConfig;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 class BootrstapTest {
 
-    private SPIClusterProviderLoader clusterProviderLoader;
-
-    @BeforeEach
-    public void setup() {
-        clusterProviderLoader = new SPIClusterProviderLoader();
-    }
+    private ClusterProviderLoader clusterProviderLoader;
 
     @Test
     public void should_throw_exception_when_no_cluster_provider() {
         ClusterConfig clusterConfig = Mockito.mock(ClusterConfig.class);
         NodeConfig nodeConfig = Mockito.mock(NodeConfig.class);
-        ClusterProviderLoader clusterProviderLoader = Mockito.mock(ClusterProviderLoader.class);
+        clusterProviderLoader = Mockito.mock(ClusterProviderLoader.class);
 
         try {
             Bootrstap cluster = new Bootrstap(clusterConfig, nodeConfig, clusterProviderLoader);
@@ -40,20 +36,30 @@ class BootrstapTest {
     }
 
     @Test
-    public void should_bootstrap_standalong_cluster() throws Exception {
-//        ClusterConfig clusterConfig = Mockito.mock(ClusterConfig.class);
-//        NodeConfig nodeConfig = Mockito.mock(NodeConfig.class);
-//        Mockito.when(clusterConfig.bootstrapDiscoveryBroadcastIntervalSeconds()).thenReturn(1L);
-//        Mockito.when(clusterConfig.clusterId()).thenReturn("testCluster");
-//        Mockito.when(clusterConfig.managementPartitions()).thenReturn(1);
-//        Mockito.when(clusterConfig.managementDataDir()).thenReturn("/tmp/atomix-mgr");
-//        Mockito.when(clusterConfig.managementGroupMembers()).thenReturn(new String[] {"local"});
-//        Mockito.when(nodeConfig.address()).thenReturn("127.0.0.1");
-//        Mockito.when(nodeConfig.port()).thenReturn(8098);
-//        Mockito.when(nodeConfig.nodeId()).thenReturn("local");
-//
-//        Bootrstap cluster = new Bootrstap(clusterConfig, nodeConfig, clusterProviderLoader);
-//        cluster.bootstrap();
-//        cluster.shutdown();
+    public void should_bootstrap_cluster() throws Exception {
+        clusterProviderLoader = Mockito.mock(ClusterProviderLoader.class);
+        Cluster cluster = Mockito.mock(Cluster.class);
+        ClusterProvider clusterProvider = Mockito.mock(ClusterProvider.class);
+        Mockito.when(clusterProvider.cluster(any(ClusterConfig.class), any(NodeConfig.class)))
+            .thenReturn(cluster);
+        Mockito.when(clusterProviderLoader.load())
+            .thenReturn(Optional.of(clusterProvider));
+        ClusterConfig clusterConfig = Mockito.mock(ClusterConfig.class);
+        NodeConfig nodeConfig = Mockito.mock(NodeConfig.class);
+        Mockito.when(clusterConfig.bootstrapDiscoveryBroadcastIntervalSeconds()).thenReturn(1L);
+        Mockito.when(clusterConfig.clusterId()).thenReturn("testCluster");
+        Mockito.when(clusterConfig.managementPartitions()).thenReturn(1);
+        Mockito.when(clusterConfig.managementDataDir()).thenReturn("/tmp/atomix-mgr");
+        Mockito.when(clusterConfig.managementGroupMembers()).thenReturn(new String[] {"local"});
+        Mockito.when(nodeConfig.address()).thenReturn("127.0.0.1");
+        Mockito.when(nodeConfig.port()).thenReturn(8098);
+        Mockito.when(nodeConfig.nodeId()).thenReturn("local");
+
+        Bootrstap clusterBootstrap = new Bootrstap(clusterConfig, nodeConfig, clusterProviderLoader);
+        clusterBootstrap.bootstrap();
+        clusterBootstrap.shutdown();
+
+        Mockito.verify(cluster).startup();
+        Mockito.verify(cluster).shutdown();
     }
 }
