e11526dd2a6417e4f3ca74318f44e4c6034b53b6
ynfeng <fynwin@gmail.com>
2020-08-11 14:43:24 +0800

    fix atomix version can't parse on github workflow.

--- cluster-atomix-provider/src/test/java/com/github/ynfeng/commander/cluster/atomix/AtomixClusterTest.java
+++ cluster-atomix-provider/src/test/java/com/github/ynfeng/commander/cluster/atomix/AtomixClusterTest.java
@@ -29,16 +29,6 @@ class AtomixClusterTest {
 
     @Test
     public void should_startup_and_shutdown_atomix_cluster() {
-        try {
-            String BUILD = Resources.toString(checkNotNull(Atomix.class.getClassLoader().getResource("VERSION"),
-                "VERSION resource is null"), StandardCharsets.UTF_8);
-            System.out.println("=========" + BUILD);
-            if(true){
-                throw new RuntimeException(BUILD) ;
-            }
-        } catch (IOException | NullPointerException e) {
-            throw new ConfigurationException("Failed to load Atomix version", e);
-        }
         ClusterConfig clusterConfig = Mockito.mock(ClusterConfig.class);
         NodeConfig nodeConfig = Mockito.mock(NodeConfig.class);
         Mockito.when(clusterConfig.bootstrapDiscoveryBroadcastIntervalSeconds()).thenReturn(1L);
