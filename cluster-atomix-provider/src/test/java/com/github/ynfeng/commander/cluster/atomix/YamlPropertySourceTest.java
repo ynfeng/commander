c5863141f6492f8061deb088b97ef41cbb5df2d2
ynfeng <fynwin@gmail.com>
2020-09-06 14:29:39 +0800

    add unit test.

--- cluster-atomix-provider/src/test/java/com/github/ynfeng/commander/cluster/atomix/YamlPropertySourceTest.java
+++ cluster-atomix-provider/src/test/java/com/github/ynfeng/commander/cluster/atomix/YamlPropertySourceTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class YamlPropertySourceTest {
 
     @Test
-    public void should_load_default_config_file_when_assign_profile() {
+    public void should_load_config_file_when_assign_profile() {
         System.setProperty("cmder.profile", "test");
         YamlPropertySource propertySource = new YamlPropertySource();
 
@@ -17,4 +17,13 @@ class YamlPropertySourceTest {
         assertThat(clusterId, is("test"));
     }
 
+    @Test
+    public void should_load_default_config_file_when_not_assign_profile() {
+        System.setProperty("cmder.profile", "");
+        YamlPropertySource propertySource = new YamlPropertySource();
+
+        String clusterId = propertySource.getProperty("cluster.id");
+
+        assertThat(clusterId, is("standalong"));
+    }
 }
