121676f89b96dee7403b5a8e45235622ca37b1e8
ynfeng <fynwin@gmail.com>
2020-12-04 15:20:22 +0800

    ready to rewrite cluster.

--- dist/src/main/java/com/github/ynfeng/commander/Main.java
+++ dist/src/main/java/com/github/ynfeng/commander/Main.java
@@ -1,15 +1,12 @@
 package com.github.ynfeng.commander;
 
-import com.github.ynfeng.commander.bootstrap.CommanderServer;
-import com.github.ynfeng.commander.cluster.SPIClusterProviderLoader;
-
 public final class Main {
     private Main() {
 
     }
 
     public static void main(String[] args) throws Exception {
-        CommanderServer bootrstap = new CommanderServer(new SPIClusterProviderLoader());
-        bootrstap.bootstrap();
+//        CommanderServer bootrstap = new CommanderServer(new SPIClusterProviderLoader());
+//        bootrstap.bootstrap();
     }
 }
--- server/src/main/java/com/github/ynfeng/commander/bootstrap/ServerException.java
+++
@@ -1,7 +0,0 @@
-package com.github.ynfeng.commander.bootstrap;
-
-public class ServerException extends RuntimeException {
-    public ServerException(String msg) {
-        super(msg);
-    }
-}
