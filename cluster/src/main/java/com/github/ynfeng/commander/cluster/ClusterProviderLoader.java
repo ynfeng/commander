aeaa7c10cebc398bde7976a0a8e1082f7aa19c0a
ynfeng <fynwin@gmail.com>
2020-12-04 15:21:28 +0800

    ready to rewrite cluster.

--- cluster/src/main/java/com/github/ynfeng/commander/cluster/ClusterProviderLoader.java
+++
@@ -1,7 +0,0 @@
-package com.github.ynfeng.commander.cluster;
-
-import java.util.Optional;
-
-public interface ClusterProviderLoader {
-    Optional<ClusterProvider> load();
-}
--- cluster/src/main/java/com/github/ynfeng/commander/cluster/Partition.java
+++
@@ -1,9 +0,0 @@
-package com.github.ynfeng.commander.cluster;
-
-public class Partition {
-    private final String name;
-
-    public Partition(String name) {
-        this.name = name;
-    }
-}
