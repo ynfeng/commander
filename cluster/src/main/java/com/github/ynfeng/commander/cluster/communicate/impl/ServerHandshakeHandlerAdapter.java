2dcb07b91893ca9c68c6ca93097266603f6dc039
ynfeng <fynwin@gmail.com>
2020-12-16 19:41:45 +0800

    some log.

--- cluster/src/main/java/com/github/ynfeng/commander/cluster/communicate/impl/ServerHandshakeHandlerAdapter.java
+++ cluster/src/main/java/com/github/ynfeng/commander/cluster/communicate/impl/ServerHandshakeHandlerAdapter.java
@@ -1,19 +1,24 @@
 package com.github.ynfeng.commander.cluster.communicate.impl;
 
 import com.github.ynfeng.commander.cluster.communicate.protocol.ProtocolVersion;
+import com.github.ynfeng.commander.support.logger.CmderLogger;
+import com.github.ynfeng.commander.support.logger.CmderLoggerFactory;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import java.util.Optional;
 
 public class ServerHandshakeHandlerAdapter extends AbstractHandshakeHanderAdapter {
+    private final CmderLogger logger = CmderLoggerFactory.getSystemLogger();
+
     public ServerHandshakeHandlerAdapter() {
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ByteBuf byteBuf = (ByteBuf) msg;
-        Optional<ProtocolVersion> clientProtocolVersionOptional = readProtocolVersion(byteBuf);
+        Optional<ProtocolVersion> clientProtocolVersionOptional = readProtocolVersion((ByteBuf) msg);
         if (clientProtocolVersionOptional.isPresent()) {
+            logger.debug("Writing server protocol version {} for connection to {}",
+                clientProtocolVersionOptional.get(), ctx.channel().remoteAddress());
             writeProtocolVersion(ctx, clientProtocolVersionOptional.get());
         } else {
             ctx.close();
