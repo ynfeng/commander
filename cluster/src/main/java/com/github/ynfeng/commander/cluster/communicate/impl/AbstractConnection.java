90f6558170d1d679c2e54f9d47e8c5c4442abbdd
ynfeng <fynwin@gmail.com>
2020-12-22 20:28:12 +0800

    exception handle.

--- cluster/src/main/java/com/github/ynfeng/commander/cluster/communicate/impl/AbstractConnection.java
+++ cluster/src/main/java/com/github/ynfeng/commander/cluster/communicate/impl/AbstractConnection.java
@@ -1,6 +1,7 @@
 package com.github.ynfeng.commander.cluster.communicate.impl;
 
 import io.netty.channel.Channel;
+import io.netty.util.concurrent.Future;
 import java.util.concurrent.CompletableFuture;
 
 public class AbstractConnection implements Connection {
@@ -12,11 +13,18 @@ public class AbstractConnection implements Connection {
         this.handlers = handlers;
     }
 
+    private static void complete(CompletableFuture<Void> completableFuture, Future<? super Void> c) {
+        if (c.isSuccess()) {
+            completableFuture.complete(null);
+        } else {
+            completableFuture.completeExceptionally(c.cause());
+        }
+    }
+
     @Override
     public CompletableFuture<Void> sendAsync(ProtocolMessage message) {
         CompletableFuture<Void> completableFuture = new CompletableFuture<>();
-        channel.writeAndFlush(message).syncUninterruptibly();
-        completableFuture.complete(null);
+        channel.writeAndFlush(message).addListener(c -> complete(completableFuture, c));
         return completableFuture;
     }
 
