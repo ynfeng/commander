274b7b66f4e0ebd606a83f0c5a39da21044c0908
ynfeng <fynwin@gmail.com>
2021-04-19 10:12:58 +0800

    deal with code smell.

--- definition/src/main/java/com/github/ynfeng/commander/definition/NodeDefinition.java
+++ definition/src/main/java/com/github/ynfeng/commander/definition/NodeDefinition.java
@@ -2,12 +2,7 @@ package com.github.ynfeng.commander.definition;
 
 @FunctionalInterface
 public interface NodeDefinition {
-    NodeDefinition NULL = new NodeDefinition() {
-        @Override
-        public String refName() {
-            return "NULL_NODE";
-        }
-    };
+    NodeDefinition NULL = () -> "NULL_NODE";
 
     String refName();
 }
