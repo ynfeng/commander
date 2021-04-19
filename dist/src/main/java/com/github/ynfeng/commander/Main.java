fda3fdfd5e7ad0dd532996b13755fd6812817e43
ynfeng <fynwin@gmail.com>
2021-04-19 20:13:30 +0800

    deal with code smell.

--- dist/src/main/java/com/github/ynfeng/commander/Main.java
+++ dist/src/main/java/com/github/ynfeng/commander/Main.java
@@ -2,11 +2,8 @@ package com.github.ynfeng.commander;
 
 public final class Main {
     private Main() {
-
     }
 
     public static void main(String[] args) throws Exception {
-//        CommanderServer bootrstap = new CommanderServer(new SPIClusterProviderLoader());
-//        bootrstap.bootstrap();
     }
 }
