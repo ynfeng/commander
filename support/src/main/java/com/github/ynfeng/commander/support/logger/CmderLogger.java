cba265f4b1a66c9b15b5dcdd884452f8f2d0327b
ynfeng <fynwin@gmail.com>
2021-04-20 20:50:37 +0800

    deal with code smell.

--- support/src/main/java/com/github/ynfeng/commander/support/logger/CmderLogger.java
+++ support/src/main/java/com/github/ynfeng/commander/support/logger/CmderLogger.java
@@ -23,37 +23,27 @@ public class CmderLogger implements Logger {
 
     @Override
     public void trace(String msg) {
-        if (isTraceEnabled()) {
-            logger.trace(msg);
-        }
+        logger.trace(msg);
     }
 
     @Override
     public void trace(String format, Object arg) {
-        if (isTraceEnabled()) {
-            logger.trace(format, arg);
-        }
+        logger.trace(format, arg);
     }
 
     @Override
     public void trace(String format, Object arg1, Object arg2) {
-        if (isTraceEnabled()) {
-            logger.trace(format, arg1, arg2);
-        }
+        logger.trace(format, arg1, arg2);
     }
 
     @Override
     public void trace(String format, Object... arguments) {
-        if (isTraceEnabled()) {
-            logger.trace(format, arguments);
-        }
+        logger.trace(format, arguments);
     }
 
     @Override
     public void trace(String msg, Throwable t) {
-        if (isTraceEnabled()) {
-            logger.trace(msg, t);
-        }
+        logger.trace(msg, t);
     }
 
     @Override
@@ -63,37 +53,27 @@ public class CmderLogger implements Logger {
 
     @Override
     public void trace(Marker marker, String msg) {
-        if (isTraceEnabled()) {
-            logger.trace(marker, msg);
-        }
+        logger.trace(marker, msg);
     }
 
     @Override
     public void trace(Marker marker, String format, Object arg) {
-        if (isTraceEnabled()) {
-            logger.trace(marker, format, arg);
-        }
+        logger.trace(marker, format, arg);
     }
 
     @Override
     public void trace(Marker marker, String format, Object arg1, Object arg2) {
-        if (isTraceEnabled()) {
-            logger.trace(marker, format, arg1, arg2);
-        }
+        logger.trace(marker, format, arg1, arg2);
     }
 
     @Override
     public void trace(Marker marker, String format, Object... argArray) {
-        if (isTraceEnabled()) {
-            logger.trace(marker, format, argArray);
-        }
+        logger.trace(marker, format, argArray);
     }
 
     @Override
     public void trace(Marker marker, String msg, Throwable t) {
-        if (isTraceEnabled()) {
-            logger.trace(marker, msg, t);
-        }
+        logger.trace(marker, msg, t);
     }
 
     @Override
@@ -103,37 +83,27 @@ public class CmderLogger implements Logger {
 
     @Override
     public void debug(String msg) {
-        if (isDebugEnabled()) {
-            logger.debug(msg);
-        }
+        logger.debug(msg);
     }
 
     @Override
     public void debug(String format, Object arg) {
-        if (isDebugEnabled()) {
-            logger.debug(format, arg);
-        }
+        logger.debug(format, arg);
     }
 
     @Override
     public void debug(String format, Object arg1, Object arg2) {
-        if (isDebugEnabled()) {
-            logger.debug(format, arg1, arg2);
-        }
+        logger.debug(format, arg1, arg2);
     }
 
     @Override
     public void debug(String format, Object... arguments) {
-        if (isDebugEnabled()) {
-            logger.debug(format, arguments);
-        }
+        logger.debug(format, arguments);
     }
 
     @Override
     public void debug(String msg, Throwable t) {
-        if (isDebugEnabled()) {
-            logger.debug(msg, t);
-        }
+        logger.debug(msg, t);
     }
 
     @Override
@@ -143,37 +113,27 @@ public class CmderLogger implements Logger {
 
     @Override
     public void debug(Marker marker, String msg) {
-        if (isDebugEnabled()) {
-            logger.debug(marker, msg);
-        }
+        logger.debug(marker, msg);
     }
 
     @Override
     public void debug(Marker marker, String format, Object arg) {
-        if (isErrorEnabled()) {
-            logger.debug(marker, format, arg);
-        }
+        logger.debug(marker, format, arg);
     }
 
     @Override
     public void debug(Marker marker, String format, Object arg1, Object arg2) {
-        if (isDebugEnabled()) {
-            logger.debug(marker, format, arg1, arg2);
-        }
+        logger.debug(marker, format, arg1, arg2);
     }
 
     @Override
     public void debug(Marker marker, String format, Object... arguments) {
-        if (isDebugEnabled()) {
-            logger.debug(marker, format, arguments);
-        }
+        logger.debug(marker, format, arguments);
     }
 
     @Override
     public void debug(Marker marker, String msg, Throwable t) {
-        if (isDebugEnabled()) {
-            logger.debug(marker, msg, t);
-        }
+        logger.debug(marker, msg, t);
     }
 
     @Override
@@ -193,176 +153,166 @@ public class CmderLogger implements Logger {
 
     @Override
     public void info(String format, Object arg1, Object arg2) {
-        if (isInfoEnabled()) {
-            logger.info(format, arg1, arg2);
-        }
+        logger.info(format, arg1, arg2);
     }
 
     @Override
     public void info(String format, Object... arguments) {
-        if (logger.isInfoEnabled()) {
-            logger.info(format, arguments);
-        }
+        logger.info(format, arguments);
     }
 
     @Override
     public void info(String msg, Throwable t) {
-        if (logger.isInfoEnabled()) {
-            logger.info(msg, t);
-        }
+        logger.info(msg, t);
     }
 
     @Override
     public boolean isInfoEnabled(Marker marker) {
-        return false;
+        return logger.isInfoEnabled();
     }
 
     @Override
     public void info(Marker marker, String msg) {
-
+        logger.info(marker, msg);
     }
 
     @Override
     public void info(Marker marker, String format, Object arg) {
-
+        logger.info(marker, format, arg);
     }
 
     @Override
     public void info(Marker marker, String format, Object arg1, Object arg2) {
-
+        logger.info(marker, format, arg1, arg2);
     }
 
     @Override
     public void info(Marker marker, String format, Object... arguments) {
-
+        logger.info(marker, format, arguments);
     }
 
     @Override
     public void info(Marker marker, String msg, Throwable t) {
-
+        logger.info(marker, msg, t);
     }
 
     @Override
     public boolean isWarnEnabled() {
-        return false;
+        return logger.isWarnEnabled();
     }
 
     @Override
     public void warn(String msg) {
-
+        logger.warn(msg);
     }
 
     @Override
     public void warn(String format, Object arg) {
-
+        logger.warn(format, arg);
     }
 
     @Override
     public void warn(String format, Object... arguments) {
-
+        logger.warn(format, arguments);
     }
 
     @Override
     public void warn(String format, Object arg1, Object arg2) {
-
+        logger.warn(format, arg1, arg2);
     }
 
     @Override
     public void warn(String msg, Throwable t) {
-
+        logger.warn(msg, t);
     }
 
     @Override
     public boolean isWarnEnabled(Marker marker) {
-        return false;
+        return logger.isWarnEnabled();
     }
 
     @Override
     public void warn(Marker marker, String msg) {
-
+        logger.warn(marker, msg);
     }
 
     @Override
     public void warn(Marker marker, String format, Object arg) {
-
+        logger.warn(marker, format, arg);
     }
 
     @Override
     public void warn(Marker marker, String format, Object arg1, Object arg2) {
-
+        logger.warn(marker, format, arg1, arg2);
     }
 
     @Override
     public void warn(Marker marker, String format, Object... arguments) {
-
+        logger.warn(marker, format, arguments);
     }
 
     @Override
     public void warn(Marker marker, String msg, Throwable t) {
-
+        logger.warn(marker, msg, t);
     }
 
     @Override
     public boolean isErrorEnabled() {
-        return false;
+        return logger.isErrorEnabled();
     }
 
     @Override
     public void error(String msg) {
-
+        logger.error(msg);
     }
 
     @Override
     public void error(String format, Object arg) {
-
+        logger.error(format, arg);
     }
 
     @Override
     public void error(String format, Object arg1, Object arg2) {
-
+        logger.error(format, arg1, arg2);
     }
 
     @Override
     public void error(String format, Object... arguments) {
-        if (logger.isErrorEnabled()) {
-            logger.error(format, arguments);
-        }
+        logger.error(format, arguments);
     }
 
     @Override
     public void error(String msg, Throwable t) {
-        if (logger.isErrorEnabled()) {
-            logger.error(msg, t);
-        }
+        logger.error(msg, t);
     }
 
     @Override
     public boolean isErrorEnabled(Marker marker) {
-        return false;
+        return logger.isErrorEnabled();
     }
 
     @Override
     public void error(Marker marker, String msg) {
-
+        logger.error(marker, msg);
     }
 
     @Override
     public void error(Marker marker, String format, Object arg) {
-
+        logger.error(marker, format, arg);
     }
 
     @Override
     public void error(Marker marker, String format, Object arg1, Object arg2) {
-
+        logger.error(marker, format, arg1, arg2);
     }
 
     @Override
     public void error(Marker marker, String format, Object... arguments) {
-
+        logger.error(marker, format, arguments);
     }
 
     @Override
     public void error(Marker marker, String msg, Throwable t) {
-
+        logger.error(marker, msg, t);
     }
 }
