5cbd929b3be450cb9bbdba775c0a77d7f5b4b668
ynfeng <fynwin@gmail.com>
2021-05-07 21:44:08 +0800

    refactor

--- raft/src/main/java/com/github/ynfeng/commander/raft/roles/Follower.java
+++ raft/src/main/java/com/github/ynfeng/commander/raft/roles/Follower.java
@@ -1,14 +1,11 @@
 package com.github.ynfeng.commander.raft.roles;
 
 import com.github.ynfeng.commander.raft.RaftContext;
-import com.github.ynfeng.commander.raft.protocol.RequestVote;
-import com.github.ynfeng.commander.raft.protocol.RequestVoteResponse;
 
-public class Follower implements RaftRole {
-    private final RaftContext raftContext;
+public class Follower extends AbstratRaftRole {
 
     public Follower(RaftContext raftContext) {
-        this.raftContext = raftContext;
+        super(raftContext);
     }
 
     @Override
@@ -19,10 +16,5 @@ public class Follower implements RaftRole {
     @Override
     public void destory() {
         //do nothing
-    }
-
-    @Override
-    public RequestVoteResponse handleRequestVote(RequestVote requestVote) {
-        return RequestVoteResponse.voted(raftContext.currentTerm(), raftContext.localMermberId());
     }
 }
